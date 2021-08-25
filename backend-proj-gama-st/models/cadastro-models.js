9d677d720e92bebcd3df0bbd55d6b647a490a78d
Leticia-R <ramos.marialeticiaalves@gmail.com>
2021-08-25 18:11:47 -0300

    criação do server, routes e schema do bd

---
+++ backend-proj-gama-st/models/cadastro-models.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+
+const registerTemplate = new mongoose.Schema({
+  nomeCompleto: {
+    type: String,
+    required: true,
+  },
+  dataNascimento: {
+    type: String,
+    required: true,
+  },
+  logradouro: {
+    type: String,
+    required: true,
+  },
+  numero: {
+    type: String,
+    required: true,
+  },
+  bairro: {
+    type: String,
+    required: true,
+  },
+  cidade: {
+    type: String,
+    required: true,
+  },
+  estado: {
+    type: String,
+    required: true,
+  },
+  cep: {
+    type: String,
+    required: true,
+  },
+  telefoneFixo: {
+    type: String,
+    required: false,
+  },
+  celular: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cargoPretendido: {
+    type: String,
+    required: true,
+  },
+  estadoCivil: {
+    type: String,
+    required: false,
+  },
+  genero: {
+    type: String,
+    required: true,
+  },
+  identidade: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+  },
+  veiculo: {
+    type: Boolean,
+    required: false,
+  },
+  habilitacao: {
+    type: Boolean,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('cadastro-candidatos', registerTemplate);
---
+++ backend-proj-gama-st/routes/routes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const registerTemps = require(".models/cadastro-models");
+
+router.post("/cadastro", (req, res) => {
+  const usuarioCadastrado = new registerTemps({
+    nomeCompleto: req.body.nomeCompleto,
+    dataNascimento: req.body.dataNascimento,
+    logradouro: req.body.logradouro,
+    numero: req.body.numero,
+    bairro: req.body.bairro,
+    cidade: req.body.cidade,
+    estado: req.body.estado,
+    cep: req.body.cep,
+    telefoneFixo: req.body.telefoneFixo,
+    celular: req.body.celular,
+    email: req.body.celular,
+    cargoPretendido: req.body.cargoPretendido,
+    estadoCivil: req.body.estadoCivil,
+    genero: req.body.genero,
+    identidade: req.body.identidade,
+    cpf: req.body.cpf,
+    veiculo: req.body.veiculo,
+    habilitacao: req.body.habilitacao,
+    password: req.body.password,
+  });
+});
+
+module.exports = router;
