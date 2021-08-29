641b43a3122993563f7c94dd9b3a4e1005b140a8
Leticia-R <ramos.marialeticiaalves@gmail.com>
2021-08-28 23:50:39 -0300

    configurada porta do heroku

--- backend-proj-gama-st/server.js
+++ backend-proj-gama-st/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 mongoose.connect(process.env.DB_ACCESS, () => console.info("BD conectado com sucesso"));
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use('/jobsNet-api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
